1df41624d5a109954ed606847efd85f1068b8cc1
de-meyer <dmeyer.private@gmail.com>
2024-08-09 23:09:30 +0200

    implement first header

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -8,25 +8,33 @@ export default async function Page({ params }: { params: { id: string } }) {
     direction: "desc",
     type: "owner",
   });
+  const { data: user } = await octokit.rest.users.getByUsername({
+    username: params.id,
+  });
 
   return (
     <main className=" min-h-screen items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white ">
-      <div className="grid grid-cols-12 flex-col py-4">
-        <div className="col-span-7"></div>
-        <div className="col-span-3 flex flex-col gap-4">
-          <Searchfield />
-        </div>
-      </div>
-      <div className="grid grid-cols-12 flex-col ">
-        <div className="col-span-2"></div>
-
-        <div className="col-span-8 flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-          <h1 className="text-5xl font-extrabold tracking-tight text-white ">
+      <header className="flex justify-between">
+        <div>
+          <h2 className="font-extrabold tracking-tight text-white ">
             Repositories of:{" "}
+            <img
+              className="inline h-5 w-5 rounded-full"
+              src={user.avatar_url}
+              alt=""
+            />{" "}
             <span className="text-[hsl(280,100%,70%)]">
               {params.id.toUpperCase()}
             </span>
-          </h1>
+          </h2>
+        </div>
+        <div>
+          <Searchfield />
+        </div>
+      </header>
+      <div className="grid grid-cols-12 flex-col ">
+        <div className="col-span-2"></div>
+        <div className="col-span-8 flex flex-col items-center justify-center gap-12 px-4 py-16 ">
           <div className="grid gap-4 lg:grid-cols-3">
             {listOfRepos.map((repo) => (
               <Tile repo={repo} key={repo.id} />
