7cb4e46d8aecb832a8705f7648de9c1c9317c61b
de-meyer <dmeyer.private@gmail.com>
2024-08-24 23:51:43 +0200

    style: change colors on the repositories page

--- src/app/[id]/repositories/Tile.tsx
+++ src/app/[id]/repositories/Tile.tsx
@@ -6,13 +6,13 @@ type props = {
 
 export default function Tile({ repo }: props) {
   return (
-    <div className="col-span-3 rounded-lg shadow-md transition-all duration-300 hover:-translate-y-1 hover:shadow-xl hover:shadow-violet-500/40">
+    <div className="col-span-3 rounded-lg border-2 border-violet-500  shadow-md transition-all duration-300 hover:-translate-y-1 hover:shadow-xl hover:shadow-violet-500/40">
       {
         <a
           href={repo.html_url}
           target="_blank"
           rel="noreferrer"
-          className="relative flex h-full flex-col rounded-lg bg-white bg-clip-border text-gray-700 shadow-md"
+          className="relative flex h-full flex-col rounded-lg  bg-clip-border  shadow-md"
           key={repo.id}
         >
           <article className="p-6">
